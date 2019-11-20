78d91d47bddf262ca02b01648f3276ae240e2648
金融研发-章辉(zhanghui1) <33326898+Roilka@users.noreply.github.com>
2019-11-20 13:19:40 +0800

    项目初始化

---
+++ api/src/test/java/com/roilka/active/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roilka.active.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
